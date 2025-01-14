a63ddd1471eca1b8f177350aa512541801b0faa3
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-14 00:26:42 -0300

    Upload de arquivos no Adminjs

--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -4,7 +4,7 @@ import { ResourceWithOptions } from "adminjs";
 import { Category, Course, Episode } from "../../models";
 import { categoryResourceOptions } from "./category";
 import { courseResourceOptions } from "./course";
-import { episodeResourceOptions } from "./episode";
+import { episodeResourceFeatures, episodeResourceOptions } from './episode';
 
 export const adminJsResources: ResourceWithOptions[] = [
   {
@@ -13,7 +13,8 @@ export const adminJsResources: ResourceWithOptions[] = [
   },
   {
     resource: Episode,
-    options: episodeResourceOptions
+    options: episodeResourceOptions,
+    features: episodeResourceFeatures
   },
   {
     resource: Category,
