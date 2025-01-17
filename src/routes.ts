df2296a23c8b90d8646cec5f258a922af05986a7
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-16 23:46:37 -0300

    Registro de usuários

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 // src/routes.ts
 
 import express from 'express'
+import { authController } from './controllers/authController'
 import { categoriesController } from './controllers/categoriesController'
 import { coursesController } from './controllers/coursesController'
 import { episodesController } from './controllers/episodesController'
 
 const router = express.Router()
+
+router.post('/auth/register', authController.register)
 
 router.get('/categories', categoriesController.index)
 router.get('/categories/:id', categoriesController.show)
@@ -16,5 +19,7 @@ router.get('/courses/search', coursesController.search)
 router.get('/courses/:id', coursesController.show)
 
 router.get('/episodes/stream', episodesController.stream)
+
+
 
 export { router }
