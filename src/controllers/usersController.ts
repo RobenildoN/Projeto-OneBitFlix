75ea9e95ee4198bcc16181356e0f7fc855d05893
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-18 11:31:38 -0300

    Atualizando informações do usuário

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -5,31 +5,53 @@ import { AuthenticatedRequest } from "../middlewares/auth";
 import { userService } from "../services/userService";
 
 export const usersController = {
-
-   // GET /users/current
-   show: async (req: AuthenticatedRequest, res: Response) => {
-    const currentUser = req.user
+  
+  // GET /users/current
+  show: async (req: AuthenticatedRequest, res: Response) => {
+    const currentUser = req.user;
 
     try {
-      return res.json(currentUser)
+      return res.json(currentUser);
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
+        return res.status(400).json({ message: err.message });
       }
     }
   },
-  
-  // GET /users/current/watching
-  watching: async (req: AuthenticatedRequest, res: Response) => {
-    const { id } = req.user!
+
+  // PUT /users/current
+  update: async (req: AuthenticatedRequest, res: Response) => {
+    const { id } = req.user!;
+    const { firstName, lastName, phone, email, birth } = req.body;
 
     try {
-      const watching = await userService.getKeepWatchingList(id)
-      return res.json(watching)
+      const updatedUser = await userService.update(id, {
+        firstName,
+        lastName,
+        phone,
+        email,
+        birth,
+      });
+
+      return res.json(updatedUser);
     } catch (err) {
       if (err instanceof Error) {
-        return res.status(400).json({ message: err.message })
+        return res.status(400).json({ message: err.message });
       }
     }
-  }
-}
+  },
+
+  // GET /users/current/watching
+  watching: async (req: AuthenticatedRequest, res: Response) => {
+    const { id } = req.user!;
+
+    try {
+      const watching = await userService.getKeepWatchingList(id);
+      return res.json(watching);
+    } catch (err) {
+      if (err instanceof Error) {
+        return res.status(400).json({ message: err.message });
+      }
+    }
+  },
+};
