9e89ec3e5fab388b8e12ab9774ae143af46f0b9b
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-21 01:43:19 -0300

    Finalização da estrutura dos slides, visual e responsividade - Seçao dos slides

--- onebitflix-front/src/components/common/slideComponent/index.tsx
+++ onebitflix-front/src/components/common/slideComponent/index.tsx
@@ -8,15 +8,42 @@ interface props {
   }
   
   const SlideComponent = function ({ course }: props) {
+	let slideCount = 0;
+
+if (course.length > 4) {
+  slideCount = 4;
+} else if (course) {
+   slideCount = course.length;
+}
 	return (
 	  <>
-		<div>
+		<div className="d-flex flex-column align-items-center py-4">
 		  <Splide
 			options={{
 			  type: "loop",
 			  perPage: 4,
 			  perMove: 1,
+			  width: 1200,
 			  pagination: false,
+			  arrows: course.length > 4 ? true : false,
+			  drag: course.length > 4 ? true : false,
+			breakpoints: {
+				1200: {
+					perPage: slideCount >= 2 ? 2 : 1,
+					width: slideCount >= 2 ? 640 : 300,
+					arrows: course.length > 2 ? true : false,
+				  drag: course.length > 2 ? true : false,
+				},
+				640: {
+					perPage: 300,
+					width: 1,
+					arrows: course.length > 1 ? true : false,
+				  drag: course.length > 1 ? true : false,
+				},
+			  300: {
+					  width: 250,
+			  },
+			  },
 			}}
 		  >
 			{course?.map((course) => (
