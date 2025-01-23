5e0f34323efceba21223a375c006c98fe885ea39
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-22 23:09:40 -0300

    Criação dos elementos do header genérico, do seu estilo visual e também da sua responsividade completa - Header genérico

--- onebitflix-front/pages/index.tsx
+++ onebitflix-front/pages/index.tsx
@@ -6,15 +6,20 @@ import CardsSection from "../src/components/homeNoAuth/cardsSection";
 import SlideSection from "@/components/homeNoAuth/slideSection";
 import { GetStaticProps } from "next";
 import courseService, { CourseType } from "../src/services/courseService";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import Footer from "../src/components/common/footer";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 interface IndexPageProps {
   children?: ReactNode;
   course: CourseType[];
 }
 
 const HomeNotAuth = function ({ course }: IndexPageProps) {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+  
   return (
     <>
       <Head>
@@ -24,12 +29,16 @@ const HomeNotAuth = function ({ course }: IndexPageProps) {
 				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil."/>
       </Head>
       <main>
-      <div className={styles.sectionBackground}>
+      <div className={styles.sectionBackground} data-aos="fade-zoom-in" data-aos-duration="1600">
 	    <HeaderNoAuth />
       <PresentationSection />
 		</div>
+    <div data-aos="fade-right" data-aos-duration="1200">
     <CardsSection />
+    </div>
+    <div data-aos="fade-up" data-aos-duration="1350">
 		<SlideSection newestCourses={course} />
+    </div>
     <Footer />
       </main>
     </>
