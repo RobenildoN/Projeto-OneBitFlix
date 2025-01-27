e5366140df9a12ac2fd7e06a35fe5290e86e4115
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-26 22:48:33 -0300

    Criação dos elementos da seção de favoritos, do seu estilo visual e também dos métodos de favorito - Seção favoritos

---
+++ onebitflix-front/src/components/homeAuth/favoriteCategory/index.tsx
@@ -0,0 +1,33 @@
+import courseService from "../../../services/courseService";
+import styles from "../../../../styles/slideCategory.module.scss";
+import useSWR from "swr";
+import SlideComponent from "@/components/common/slideComponent";
+
+const FavoritesCourses = function () {
+  const { data, error } = useSWR("/favCourses", courseService.getFavCourses);
+
+  if (error) return error;
+  //if (!data) return <SwrSpinner />;
+  if (!data)
+    return (
+      <>
+        <p>Loading...</p>
+      </>
+    );
+
+  return (
+    <>
+      {" "}
+      <p className={styles.titleCategory}>Minha Lista</p>
+      {data.data.courses.length >= 1 ? (
+	<SlideComponent course={data.data.courses} />
+) : (
+  <p className="h5 text-center pt-3">
+	  <strong>Você não tem nenhum curso na lista</strong>
+  </p>
+)}
+    </>
+  );
+};
+
+export default FavoritesCourses;
