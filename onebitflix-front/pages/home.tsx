03c9bbf1ebc51b3caf1eeedd5953a25ee40bc520
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-26 23:28:38 -0300

    Criação da página home autenticada, do seu estilo visual e também da sua responsividade completa. Implementação de todas as categorias na página e criação de seriços - Home autenticada

--- onebitflix-front/pages/home.tsx
+++ onebitflix-front/pages/home.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import FavoriteCategory from "@/components/homeAuth/favoriteCategory";
 import FeaturedCategory from "@/components/homeAuth/featuredCategory";
 import ListCategories from "@/components/homeAuth/listCategories";
+import Footer from "@/components/common/footer";
 
 const HomeAuth = function () {
   return (
@@ -18,6 +19,7 @@ const HomeAuth = function () {
         <FavoriteCategory />
         <FeaturedCategory />
         <ListCategories />
+        <Footer />
       </main>
     </>
   );
