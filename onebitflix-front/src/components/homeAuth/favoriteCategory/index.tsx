4e80d8c247463f9f82bf99866f282dee615a115f
Robenildo <robenildo_noberto@yahoo.com.br>
2025-01-26 22:56:29 -0300

    Criação dos elementos da seção de destaque, do seu estilo visual - Seção destaque

--- onebitflix-front/src/components/homeAuth/favoriteCategory/index.tsx
+++ onebitflix-front/src/components/homeAuth/favoriteCategory/index.tsx
@@ -18,7 +18,7 @@ const FavoritesCourses = function () {
   return (
     <>
       {" "}
-      <p className={styles.titleCategory}>Minha Lista</p>
+      <p className={styles.titleCategory}>MINHA LISTA</p>
       {data.data.courses.length >= 1 ? (
 	<SlideComponent course={data.data.courses} />
 ) : (
