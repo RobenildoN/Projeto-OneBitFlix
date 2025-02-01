78e808c25426692559c5b3e39ee30ab89a66a641
Robenildo <robenildo_noberto@yahoo.com.br>
2025-02-01 03:00:32 -0300

    Criação dos elementos de search, do seu estilo visual e também da sua responsividade completa - Página de search

--- onebitflix-front/pages/search.tsx
+++ onebitflix-front/pages/search.tsx
@@ -4,19 +4,26 @@ import HeaderAuth from "../src/components/common/headerAuth";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import courseService, { CourseType } from "@/services/courseService";
+import { Container } from "reactstrap";
+import SearchCard from "@/components/searchCard";
+import Footer from "@/components/common/footer";
 
 const Search = function () {
     const router = useRouter();
 const searchName: any = router.query.name;
 const [searchResult, setSearchResult] = useState<CourseType[]>([]);
 
-const searchCourses = async function () {
-	if (searchName === "string") {
-	  const res = await courseService.getSearch(searchName);
+const searchCourses = async () => {
+    if (typeof searchName === "string") {
+      const res = await courseService.getSearch(searchName);
 
-    setSearchResult(res.data.courses);
-  }
-};
+      setSearchResult(res.data.courses);
+
+      if (res.data.courses.length === 0) {
+        //(false);
+      }
+    }
+  };
 
 useEffect(() => {
 	searchCourses();
@@ -28,13 +35,24 @@ useEffect(() => {
         <title>Onebitflix - {searchName}</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
-      <main>
+      <main className={styles.main}>
+      <div className={styles.headerFooterBg}>
         <HeaderAuth />
-        {searchResult?.map((course) => (
-	  <div key={course.id}>
-	    <p>{course.name}</p>
-    </div>
-   ))}
+        </div>
+        {searchResult.length >= 1 ? (
+            <div className={styles.searchResult}>
+          <Container className="container d-flex flex-wrap justify-content-center gap-5 py-4">
+          {searchResult?.map((course) => (
+            <SearchCard key={course.id} course={course} />
+          ))}
+        </Container>
+        </div>
+        ) : (
+          <p className={styles.noSearchText}>Nenhum resultado encontrado!</p>
+        )}
+        <div className={styles.headerFooterBg}>
+          <Footer />
+        </div>
       </main>
     </>
   );
