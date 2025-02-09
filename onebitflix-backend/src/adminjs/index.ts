bfc1317113feb72d2a0c680bf1ada9cea46d7810
Robenildo <robenildo_noberto@yahoo.com.br>
2025-02-09 00:32:53 -0300

    Preparação do Deploy feita na mentoria

---
+++ onebitflix-backend/src/adminjs/index.ts
@@ -0,0 +1,40 @@
+import AdminJs from "adminjs";
+import AdminJsExpress from "@adminjs/express";
+import AdminJsSequelize from "@adminjs/sequelize";
+import { database } from "../database";
+import { adminJsResources } from './resources'
+import { locale } from './locale'
+import { dashboardOptions } from './dashboard'
+import { brandingOptions } from './branding'
+import { authtenticationOptions } from './authentication'
+import session from "express-session"
+import connectSession from "connect-session-sequelize"
+import { Sequelize } from "sequelize";
+import { ADMINJS_COOKIE_PASSWORD } from "../../config/environment";
+
+const SequelizeStore = connectSession(session.Store)
+const store = new SequelizeStore({ db: database })
+store.sync()
+
+AdminJs.registerAdapter(AdminJsSequelize)
+
+export const adminJs = new AdminJs({
+  databases: [database],
+  resources: adminJsResources,
+  rootPath: '/admin',
+  dashboard: dashboardOptions,
+  locale: locale,
+  branding: brandingOptions
+})
+
+export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
+  adminJs,
+  authtenticationOptions,
+  null,
+  { 
+    resave: false, 
+    saveUninitialized: false,
+    store: store,
+    secret: ADMINJS_COOKIE_PASSWORD
+   }
+)
---
+++ onebitflix-backend/src/database/index.ts
@@ -0,0 +1,8 @@
+import { Sequelize } from 'sequelize'
+import { DATABASE_URL } from '../../config/environment'
+
+export const database = new Sequelize(DATABASE_URL, {
+	define: {
+    underscored: true
+  }
+})
